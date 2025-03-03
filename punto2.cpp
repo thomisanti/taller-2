ae0c980aee09e74769b328a351ae4bbe827c5b9a
thomisanti <thomassantiago733@gmail.com>
2025-03-03 07:33:31 -0500

    Create punto2.cpp

---
+++ punto2.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream> 
+//trabajo numero 2
+using namespace std;
+
+int main() {
+
+	double temperatura;
+	cout << "cual es la temperatura que marca el sensor";
+	cin >> temperatura ;
+	cout << "la temperatura es " << temperatura << "°C" << endl;
+	
+	return 0;
+
+}
+ // calcular la temperatura 
