104ca0d9cbf5cfb89b27e371f06d53026c741753
thomisanti <thomassantiago733@gmail.com>
2025-03-03 07:35:12 -0500

    Create punto4.cpp

---
+++ punto4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+using namespace std; 
+ int main () {
+ double num1;
+ double num2;
+ 
+ cout << "ingresr el primer numero";
+ cin >> num1;
+ cout << "ingrese el segundo numero";
+ cin >> num2;
+ 
+ double suma = num1 + num2 ;
+ 
+ cout << "la suma entre esos dos numeros son " << suma << endl;
+ return 0; 
+ 
+ }
+ 
+ // sumar los dos numeros asignados 
