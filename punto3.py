1965031ddf88615fd5eb2374452709eb7b4b298c
thomisanti <thomassantiago733@gmail.com>
2025-03-03 07:34:43 -0500

    Create punto3.py

---
+++ punto3.py
@@ -0,0 +1,7 @@
+a = (float("cual es el valor de a") 
+b = (float("cual es el valor de b")
+c = (float("cual es el valor de c")
+x = (float("asigna un numero para x")
+
+	print(f"la cuadratica es + {a * (x * x) + b * x + c}") 
+
