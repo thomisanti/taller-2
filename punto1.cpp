795c6b7ed1afc42574526317634ba6511d95d83b
thomisanti <thomassantiago733@gmail.com>
2025-03-03 07:32:25 -0500

    Create punto1.cpp

---
+++ punto1.cpp
@@ -0,0 +1,16 @@
+#incluide <iostream> 
+//trabajo numero 1
+using namespace std;
+
+int main() {
+	cout<< "que producto necesita el cliente";
+
+	
+	cin >> producto;
+	
+	cout << producto << "en sus manos"<< endl;
+	
+	return 0 ;
+
+}
+// proceso: dar pan en las manos de el cliente
